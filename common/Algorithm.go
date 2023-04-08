a23eeae1131d6a55cdfb92d24f8f706223570304
caifengze <fengzcw@gmail.com>
2023-04-08 21:47:41 +0800

    feat: Algorithm part

--- common/Algorithm.go
+++ common/Algorithm.go
@@ -1,14 +1,15 @@
-package commom
+package common
 
 import (
 	"log"
 	"os/exec"
 )
 
-func AlgorithmExec() {
+func AlgorithmExec() (int, error) {
 	cmd := exec.Command("cpp", "main.cpp", "-o") // 传入参数补充
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	return 0, err
 }
