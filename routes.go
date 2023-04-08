3ce19f590679b3bcb3ed8c0ba05edc92923f5c27
caifengze <fengzcw@gmail.com>
2023-04-08 21:56:22 +0800

    feat: Update routes

--- routes.go
+++ routes.go
@@ -10,15 +10,17 @@ func CollectRoute(r *gin.Engine) *gin.Engine {
 	// GET
 	r.GET("/Hello", controller.Hello)
 	r.GET("/ByeBye", controller.Bye)
+	r.GET("/StartSchedule", controller.StartSchedule)
 
 	// POST
 	r.POST("/HandleRequest", controller.HandleRequest)
 	r.POST("/RegisterRequest", controller.RegisterRequest)
 
-	// r.POST("/DefaultValue", controller.DefaultValue)
-	// r.POST("/EmployeeManagement", controller.Manageement)
-	// r.POST("/DailySchedule"), controller.DailySchedule)
-	// r.POST("/WeeklySchedule"), controller.WeeklySchedule)
+	//r.POST("/DefaultValue", controller.DefaultValue)
+	//r.POST("/EmployeeManagement", controller.Manageement)
+	r.POST("/DailySchedule", controller.DailySchedule)
+	r.POST("/WeeklySchedule", controller.WeeklySchedule)
 
+	r.POST("/EmployeesAdd", controller.EnployeesAdd)
 	return r
 }
